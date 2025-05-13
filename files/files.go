f7c660bca2bd85e856928a235e26b4aa7046dadd
Мальцев Евгений <e.maltsev@cpilot.cognitivepilot.com>
2025-05-13 15:49:14 +0700

    goida

--- files/files.go
+++ files/files.go
@@ -5,15 +5,26 @@ import (
 	"os"
 )
 
-func ReadFile(filename string) ([]byte, error) {
-	data, err := os.ReadFile(filename)
+type JsonDb struct {
+	filename string
+}
+
+func NewJsonDb(name string) *JsonDb {
+	return &JsonDb{
+		filename: name,
+	}
+
+}
+
+func (db *JsonDb) Read() ([]byte, error) {
+	data, err := os.ReadFile(db.filename)
 	if err != nil {
 		return nil, fmt.Errorf("reading failure: %w", err)
 	}
 	return data, nil
 }
-func WriteFile(content []byte, name string) error {
-	file, err := os.Create(name)
+func (db *JsonDb) Write(content []byte) error {
+	file, err := os.Create(db.filename)
 	if err != nil {
 		return fmt.Errorf("creation failure: %w", err)
 	}
